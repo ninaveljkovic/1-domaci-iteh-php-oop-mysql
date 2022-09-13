e939951cd63bc721b9d230060f35f6ded5dc57d4
ninaveljkovic <ninaveljkovic999@gmail.com>
2022-09-13 11:41:18 +0200

    kreiran model aplikacije

---
+++ model/Frizura.php
@@ -0,0 +1,36 @@
+<?php
+
+    class Frizura{
+        private $id;
+        private $naziv;
+        private $vreme; 
+        private $cena; 
+
+
+
+        public function __construct($id=null, $naziv=null,$vreme =null, $cena=null ){
+            $this->id=$id;
+            $this->naziv=$naziv; 
+            $this->cena=$cena;  
+            $this->vreme=$vreme; 
+
+        }
+ 
+
+ 
+
+
+
+
+      
+
+
+
+
+
+    }
+
+
+
+
+?>
